6a969b34c2660da2297e2195a98c26166f606438
Raza-Naqvi <raza.naqvi@olivedigital.com>
2025-06-11 23:22:23 +0500

    version 15

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,8 +1,8 @@
-// export const BASE_URL = 'https://free99us.com/api'; //live url
-// export const APP_URL = 'https://free99us.com';
+export const BASE_URL = 'https://free99us.com/api'; //live url
+export const APP_URL = 'https://free99us.com';
 
-export const BASE_URL = 'https://dev.free99us.com/api'; //testing url
-export const APP_URL = 'https://dev.free99us.com';
+// export const BASE_URL = 'https://dev.free99us.com/api'; //testing url
+// export const APP_URL = 'https://dev.free99us.com';
 
 export const PUSHER_APP_KEY = 'b17eef1edf2329b7f6e5';
 export const PUSHER_APP_CLUSTER = 'mt1';
